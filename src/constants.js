1e2d9656efdfccacdde839c2b6a9baf6bf4df860
Dejan Toteff <deyan8284@gmail.com>
2019-05-05 17:05:42 +0300

    feat@important remove no coverage mode

--- src/constants.js
+++ src/constants.js
@@ -1,21 +1,13 @@
-const { niketaConfig } = require('./_modules/niketaConfig')
+const DEFAULT_MODE = 'WITH_COVERAGE'
 
-const DEFAULT_MODE = niketaConfig('DEFAULT_MODE')
-
-const MODES_LIST = [
+const MODES = [
+  DEFAULT_MODE,
   'LOCK_FILE',
   'LINT_ONLY',
-  'NO_COVERAGE',
-  'WITH_COVERAGE',
   'OFF',
 ]
 
-const withoutDefault = MODES_LIST.filter(x => x !== DEFAULT_MODE)
-
-exports.MODES = [
-  DEFAULT_MODE,
-  ...withoutDefault,
-]
+exports.MODES = MODES
 
 exports.START = 'niketa.start'
 exports.CHANGE_MODE = 'niketa.changeMode'
