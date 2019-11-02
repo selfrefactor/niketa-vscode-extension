76ccc8f863dd0c3c565bdb0d420da0140eb8ba3a
Dejan Toteff <deyan8284@gmail.com>
2019-11-02 23:00:54 +0200

    feat: do not emit wrong files such as json

--- packages/vscode/src/steps/saved.js
+++ packages/vscode/src/steps/saved.js
@@ -1,6 +1,8 @@
 const { setter, getter } = require('rambdax')
+const allowed = ['.js','.jsx','.tsx','.ts']
 
 function saved({ filePath, rabbitHole }){
+  if(!allowed.some(x =>filePath.endsWith(x) ) ) return
   const currentMode = getter('MODE')
 
   if (currentMode === 'OFF') return
