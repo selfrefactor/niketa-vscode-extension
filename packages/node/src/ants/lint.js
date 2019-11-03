90c51a2a3e4b6c3453b0e2ede3955d5bbe19f431
Dejan Toteff <deyan8284@gmail.com>
2019-11-03 13:08:35 +0200

    chore@dnd

--- packages/node/src/ants/lint.js
+++ packages/node/src/ants/lint.js
@@ -2,8 +2,8 @@ import { existsSync } from 'fs'
 import { lintFn } from 'lint-fn'
 
 export async function lintAnt(filePath){
-  if(existsSync(filePath)) return
-  
+  if(!existsSync(filePath)) return
+
   const logResult = await lintFn({
     filePath,
     logFlag      : true,
