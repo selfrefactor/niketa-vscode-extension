110e69957a2f53e17f866264f5c6c09a5d37a746
Dejan Toteff <deyan8284@gmail.com>
2019-11-03 13:08:23 +0200

    fix: do not lint file that is deleted

--- packages/node/src/ants/lint.js
+++ packages/node/src/ants/lint.js
@@ -1,6 +1,9 @@
+import { existsSync } from 'fs'
 import { lintFn } from 'lint-fn'
 
 export async function lintAnt(filePath){
+  if(existsSync(filePath)) return
+  
   const logResult = await lintFn({
     filePath,
     logFlag      : true,
