cebbf68586a488c701fff8fb65f3a1998551f0ae
Dejan Toteff <deyan8284@gmail.com>
2020-01-29 21:11:06 +0200

    chore@dnd

--- packages/node/index.js
+++ packages/node/index.js
@@ -1,2 +1,2 @@
-require = require("esm")(module/*, options*/)
-module.exports = require("./src/startSockets.js")
+require = require('esm')(module /*, options*/)
+module.exports = require('./src/startSockets.js')
