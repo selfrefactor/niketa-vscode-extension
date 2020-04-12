8426deb43687e4b7a2161df0c1a91ad7aeb346c2
Dejan Toteff <deyan8284@gmail.com>
2020-04-12 19:29:28 +0300

    feat@progress

--- packages/node/src/niketa-client.js
+++ packages/node/src/niketa-client.js
@@ -14,46 +14,18 @@ function isWorkFile(filePath){
 }
 
 
-export class NiketaClient{
+class NiketaClient{
   constructor(port) {
     this.port= port
     this.serverInit = false
     this.emit = (x) => console.log(x, 'emit not yet initialized')
   }
-  start(){
-    if(this.serverInit) return
-
-    const server = createServer(function(socket) {
-      log(`Server created`, 'info')
-      socket.on('data', (messageFromVSCode) => {
-        console.log({messageFromVSCode: messageFromVSCode.toString()})
-
-        const parsedMessage = tryCatch(() => JSON.parse(messageFromVSCode.toString()), false)()
-        if(parsedMessage === false){
-          return this.onWrongIncomingMessage(messageFromVSCode.toString())
-        }
-        this.onChangedFile(parsedMessage)
-      })
-  
-      this.emit = (message) => {
-        socket.write(JSON.stringify(message));
-        socket.pipe(socket);
-      }
-    });
-
-    log(`Listen at ${ this.port } for vscode`, 'back')
-    server.listen(this.port, '127.0.0.1');
-  }
-  onWrongIncomingMessage(messageFromVSCode){
-    console.log({messageFromVSCode})
-    return log('Error while parsing messageFromVSCode', 'error')
-  }
-  async onChangedFile(message){
+  onMessage(message){
     console.log({message})
-    await delay(2000)
+    // await delay(2000)
     const testData = [
       {
-        fileName: message.filePath,
+        fileName: message.fileName,
         logData: {
           1: 'foo',
           5: 'foo1',
@@ -66,4 +38,35 @@ export class NiketaClient{
     
     // this.emit({decorations})
   }
+  onSocketData(messageFromVSCode){
+    console.log({messageFromVS: messageFromVSCode.toString()})
+
+    const parsedMessage = tryCatch(() => JSON.parse(messageFromVSCode.toString()), false)()
+    if(parsedMessage === false){
+      return this.onWrongIncomingMessage(messageFromVSCode.toString())
+    } 
+    this.onMessage(parsedMessage)
+  }
+  start(){
+    if(this.serverInit) return
+    this.server = createServer(socket => {
+      log(`Server created`, 'info')
+      socket.on('data', data => this.onSocketData(data.toString()))
+  
+      this.emit = (message) => {
+        socket.write(JSON.stringify(message));
+        socket.pipe(socket);
+      }
+    });
+
+    log(`Listen at ${ this.port } for vscode`, 'back')
+    this.server.listen(this.port, '127.0.0.1');
+  }
+  onWrongIncomingMessage(messageFromVSCode){
+    console.log({messageFromVSCode})
+    return log('Error while parsing messageFromVSCode', 'error')
+  }
 }
+
+const niketaClient = new NiketaClient(3020)
+niketaClient.start()
