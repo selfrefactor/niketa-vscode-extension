4682817142751f915343cd1ac4f8982ef0b97181
Dejan Toteff <deyan8284@gmail.com>
2020-04-13 18:21:35 +0300

    feat@progress  | use execa

---
+++ packages/node/src/utils/exec-jest.js
@@ -0,0 +1,37 @@
+import execa from 'execa'
+const JEST_BIN = './node_modules/jest/bin/jest.js'
+
+export async function execJest(fileName, cwd){
+  const testPattern = `-- ${ fileName }`
+  const commandInputs = [
+    JEST_BIN,
+    // '-u',
+    // '--maxWorkers=1',
+    '--env=node',
+  testPattern
+]
+console.log('before')
+const command = `node ${JEST_BIN} ${commandInputs.join(' ')}`
+const subprocess = execa.command(command, {cwd});
+// const subprocess = execa('node', commandInputs, {cwd});
+console.log('after')
+
+	setTimeout(() => {
+    // subprocess.kill('SIGTERM', {
+    //   forceKillAfterTimeout: 2000
+    // })
+		subprocess.cancel();
+	}, 10);
+
+	try {
+    console.log('before await')
+    const result = await subprocess;
+    console.log(123)
+    return result
+	} catch (error) {
+    console.log(12)
+		console.log(subprocess.killed); // true
+    console.log(error.isCanceled);
+    return error;
+  }
+}
