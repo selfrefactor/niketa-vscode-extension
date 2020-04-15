70030e3e936469b1a447be20e1ac3888bafef7e7
Dejan Toteff <deyan8284@gmail.com>
2020-04-15 19:01:23 +0300

    chore@dnd

--- packages/node/src/modules/when-file-lose-focus.js
+++ packages/node/src/modules/when-file-lose-focus.js
@@ -4,7 +4,9 @@ import { log } from 'helpers-fn'
 
 export async function whenFileLoseFocus(filePath){
   if (!existsSync(filePath)) return
+  log('sep')
   log(`willLint ${filePath}`, 'info')
+  log('sep')
   
   await lintFn(filePath)
 }
