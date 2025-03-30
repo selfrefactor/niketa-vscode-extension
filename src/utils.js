fbac0ece34e104237c4d6583055b6e7f7d434974
Dejan Toteff <deyan8284@gmail.com>
2025-03-30 17:01:19 +0300

    feat: fix disposed terminal

--- src/utils.js
+++ src/utils.js
@@ -1,42 +1,46 @@
-const { readFileSync } = require('fs')
-const { spawn } = require('child_process')
+const { readFileSync } = require('node:fs')
+const { spawn } = require('node:child_process')
 const { window } = require('vscode')
 
 const terminalsRegistry = {}
 
-function runInVsCodeTerminal({ command, label, closeAfter}) {
-  if (!terminalsRegistry[ label ]) {
-    terminalsRegistry[ label ] = window.createTerminal({ name : label });
+function runInVsCodeTerminal({ command, label, closeAfter }) {
+  if (
+    !terminalsRegistry[label] ||
+    terminalsRegistry[label].exitStatus !== undefined
+  ) {
+    terminalsRegistry[label] = window.createTerminal({ name: label })
   }
-  terminalsRegistry[ label ].sendText(command);
-  terminalsRegistry[ label ].show(true)
+  terminalsRegistry[label].sendText(command)
+  terminalsRegistry[label].show(true)
   if (closeAfter) {
     setTimeout(() => {
-      terminalsRegistry[ label ].dispose()
-      delete terminalsRegistry[ label ]
+      terminalsRegistry[label].dispose()
+      delete terminalsRegistry[label]
     }, 2500)
   }
 }
 
 const spawnCommand = ({ command, cwd, inputs, onLog }) =>
   new Promise((resolve, reject) => {
-    const proc = spawn(
-      command, inputs, {
-        cwd,
-        env   : process.env,
-        shell : true,
-      }
-    )
+    const proc = spawn(command, inputs, {
+      cwd,
+      env: process.env,
+      shell: true,
+    })
 
     proc.stdout.on('data', chunk => {
-      if (onLog) onLog(chunk.toString())
-      else console.log(chunk.toString())
+      if (onLog) {
+        onLog(chunk.toString())
+      } else {
+        console.log(chunk.toString())
+      }
     })
     proc.stdout.on('end', () => resolve())
     proc.stdout.on('error', err => reject(err))
   })
 
-function readJson(filePath){
+function readJson(filePath) {
   const raw = readFileSync(filePath)
   const content = raw.toString()
 
